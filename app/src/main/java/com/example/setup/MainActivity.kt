8d6f100a937b39f210045bc9f4cf1f15e4dcb48a
ErjanGeekTech <erjandyushebaev875@gmail.com>
2021-08-03 19:42:13 +0300

    qr code

--- app/src/main/java/com/example/setup/MainActivity.kt
+++ app/src/main/java/com/example/setup/MainActivity.kt
@@ -1,13 +1,133 @@
 package com.example.setup
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.budiyev.android.codescanner.*
+import com.example.setup.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var codeScanner: CodeScanner
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupPermissions()
+        codeScanner()
     }
-    
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(this, binding.scn)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                runOnUiThread {
+                    if (it.text == "Geektech"
+//                        && addWifi() == "198.0.168.192"
+                    ) {
+                        binding.tvText.text = it.text
+                        addWifi()
+                    }
+                }
+            }
+
+            errorCallback = ErrorCallback {
+                runOnUiThread {
+                    Log.e("Main", "codeScanner: ${it.message}")
+                }
+            }
+
+            binding.scn.setOnClickListener {
+                codeScanner.startPreview()
+                binding.tvText.text = "Scanner"
+            }
+
+        }
+    }
+
+    fun addWifi(): String {
+        val wifiMgr: WifiManager =
+            applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+
+        val wifiInfo: WifiInfo = wifiMgr.connectionInfo
+        val cod = wifiInfo.bssid
+
+//        val ipAddress = BigInteger.valueOf(wifiInfo.ipAddress.toLong()).toByteArray()
+//        try {
+//            val myAddr = InetAddress.getByAddress(ipAddress)
+//            var hostAddr = myAddr.hostAddress
+        Toast.makeText(this, "Inet  $cod ", Toast.LENGTH_LONG).show()
+//        } catch (e: UnknownHostException) {
+//            e.printStackTrace()
+//        }
+        return ""
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQ
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_REQ -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        this,
+                        "You need the camera permission to use this app",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_REQ = 101
+    }
+
 }
